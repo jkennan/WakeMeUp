7c803e33d21abdb05ebf9ffdc1d4b6b34622140a
Warren-Partridge <maps@perdix.org>
2018-01-27 16:47:30 -0500

    Add frontend template

--- web/server.js
+++ web/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app     = express();
+var path = require('path');
+
+
+app.use(express.static(__dirname));
+
 
 // Request to main server 
 app.get('/', (req, res) => {
-    res.send('Server active');
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
+
+
 
 
 app.listen(8080, () => console.log('API running on port 8080'));
