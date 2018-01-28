5156129eefd0f8b78e764f30198f5363685670bb
zhangstephanie <30350955+zhangstephanie@users.noreply.github.com>
2018-01-28 10:01:23 -0500

    Add files via upload

--- alexa_skill/index.js
+++ alexa_skill/index.js
@@ -2,12 +2,10 @@
 
 // Import packages.
 var Alexa = require('alexa-sdk');
-//var constants = require('./constants');
 var intentHandlers = require('./intentHandlers');
 
 exports.handler = function (event, context) {
     let alexa = Alexa.handler(event, context);
-    //alexa.appId = constants.appId;
     alexa.registerHandlers(intentHandlers);
     alexa.execute();
 };
